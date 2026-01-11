5b6b8dca1cdec689f29656e81ef6f15c97dbe8b3
Goldy Suryan <goldy.suryan@builder.ai>
2026-01-11 20:43:30 +0530

    feat: global error handler added

--- backend/src/app.js
+++ backend/src/app.js
@@ -19,4 +19,18 @@ app.get('/health', (req, res) => {
   });
 });
 
+app.use((req, res, next) => {
+  const error = new Error('Not Found');
+  error.status = 404;
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    error: {
+      message: err.message || 'Something went wrong',
+    },
+  });
+})
+
 export default app;
